25012b831499c3b9e86fdfca081400b8ae28aebf
Deepikapc <deepu.deepika.pc@gmail.com>
2017-06-10 17:58:43 +0530

    adds age field to subscribers

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170610121634) do
+ActiveRecord::Schema.define(version: 20170610122648) do
 
   create_table "advertisers", force: :cascade do |t|
     t.string   "name"
